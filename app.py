b4ab60db5fa0c8539b95c90cd96460125cdf1354
galu7424 <galu7424@colorado.edu>
2021-03-10 02:19:28 -0800

    Testing routes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Keep track of visitors 
+
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
@@ -20,6 +20,19 @@ def index():
 
     # Render HTML with count variable
     return render_template("index.html", count=count)
+
+
+@app.route("/page1")
+def one():
+    return render_template("login.html")
+
+@app.route("/page2")
+def two():
+    return "Hello, World 2"
+
+@app.route("/page3")
+def three():
+    return "Hello, World 3"
 
 if __name__ == "__main__":
     app.run()
