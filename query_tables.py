6de27c585decf505196391f0316c44022bfb3c6c
Daniel Bae <danieljbaee@hotmail.com>
2021-03-30 03:37:07 -0700

    hooked foriegn keys up to projects table

--- query_tables.py
+++ query_tables.py
@@ -9,28 +9,30 @@ Goal: test the following cases with queries
 
 Note: children (Users) inherit attributes from parent(Skills) tables 
 """
-
 from webapp import db
-from webapp import Users, Projects, Skills, csField, UserProjects
+from webapp import Users, Projects, Skills, csField, ProjectInterests, UserProjects
 from sqlalchemy import or_
 
 
 # 1) convert all rows Users into: a list of objects
 # #-----------------------------------------------------------------------
+print("\nObject Relational Mappers(SQLAlchemy) represent tables as a List of Objects\n")
 user_rows = Users.query.all()
-print(f"{type(user_rows)} \n{user_rows} \n")
+print(f"user_rows type = {type(user_rows)}")
 
 for user in user_rows:
     print(f'row: {user}')
-print("-"*50)
+print("-"*100)
 
 
 # 2) Find all users names of people
-skill_python = Skills.query.filter_by(name='Python').first()
 # #-----------------------------------------------------------------------
-# Option 2A) using relationship on skills tables "users" (commented out in webapp.py)
-for person in skill_python.users:
-    print(f"python users: {person.first_name} {person.last_name}")
+skill_python = Skills.query.filter_by(name='Python').first()
+print("Query all users with skill_python as a skill\n")
+
+# Option 2A) using relationship on skills tables "users" (deleted from webapp.py)
+# for person in skill_python.users:
+#     print(f"python users: {person.first_name} {person.last_name}")
 
 # Option 2B) filtering users by skill id
 python_users = Users.query.filter(or_(Users.skill_id_1 == skill_python.id,
@@ -38,11 +40,12 @@ python_users = Users.query.filter(or_(Users.skill_id_1 == skill_python.id,
                                       Users.skill_id_3 == skill_python.id))
 for person in python_users:
     print(f"python users:{person.first_name} {person.last_name}")
+print("-"*100)
 
-
-# 3) Find the name and description of skills, for some_user
+# 3) Accessing properties
 # #-----------------------------------------------------------------------
-# Now we have full access to all of attributes of Skill
+print("for some_user, Find the name and description of skills")
+print("Now we have full access to all of attributes of Skill\n")
 some_user = Users.query.filter_by(first_name='daniel').first()
 print(some_user.skill_1)
 print(some_user.skill_1.name)
@@ -52,65 +55,51 @@ print(some_user.skill_2)
 print(some_user.skill_2.name)
 print(some_user.skill_3)
 print(some_user.skill_3.name)
-print(some_user.allskills)
-
-# secondary join to reduce the need for 3 columns for each skill
-# actors = db.relationship("Actor", secondary=actors, backref="movies", lazy="select")
+print("-"*100)
 
 
 # 4) Testing junction table
 # #-----------------------------------------------------------------------
-# Pull all users from projects tables
-# - Using .relationship() named "project_members" found on Projects table
+
+# Using .relationship() named "project_members" found on Projects table
+print("Query - Pull all users from projects tables\n")
 project_rows = Projects.query.all()
 for project in project_rows:
     print(project.__repr__())
     for idx, member in enumerate(project.project_members):
         print(f'\t i={idx}: {member.__repr__()})')
+print("-"*50)
 
-# Bi-Direction: Pull all projects a given user is on
-# - Using backref named "user_projects", ~invisble column on Users table
+# Using backref "user_projects" (~invisble column on Users table)
+print("Bi-Direction: Pull all projects a given user is on\n")
 for user in user_rows:
     print(f"{user.__repr__()}")
     for idx, project in enumerate(user.user_projects):
         print(f'\t i={idx}: {project.__repr__()})')
+print("-"*100)
 
 
-# #5) Adding new rows using pythonic lists (append, extend)
+# 5) Testing Relationships (mainlyt with Projects table)
 # #-----------------------------------------------------------------------
-# new_project = Projects(name="Machine learning noobs",
-#                        desc="looking for other noobs to develop machine learning skills with")
-# db.session.add(new_project)
-# db.session.commit()
+print("for all fields and users tagged with interest\n")
+fields_rows = csField.query.all()
+for field in fields_rows:
+    print(f"{field.__repr__()}")
+    for idx, user in enumerate(field.users):
+        print(f'\t i={idx}: {user.__repr__()})')
+print("-"*100)
 
-# skill_vim = Skills(name="Vim", desc="A text editor")
-# skill_vs = Skills(name="vscode", desc="A text editor")
-# skill_note = Skills(name="notepad", desc="A text editor")
+print("csField - for all field and all projects tagged with interest\n")
+for field in fields_rows:
+    print(field.__repr__())
+    for project in field.projects:
+        print(project.__repr__())
+print("-"*100)
 
-# new_user = Users(first_name='vim', last_name='god', email="vimgod@vim.com",
-#                  skill_id_1=skill_vim.id,
-#                  skill_proficiency_1=4,
-#                  skill_id_2=1,
-#                  skill_proficiency_2=3,
-#                  skill_id_3=2,
-#                  skill_proficiency_3=2
-#                  )
-# new_user_2 = Users(first_name='vscode', last_name='champ', email="not_vimgod@vs.com",
-#                    skill_id_1=skill_vs.id,
-#                    skill_proficiency_1=4,
-#                    skill_id_2=1,
-#                    skill_proficiency_2=3,
-#                    skill_id_3=2,
-#                    skill_proficiency_3=2
-#                    )
-# new_user_3 = Users(first_name='notepad', last_name='OG', email="whatsvim@og.com",
-#                    skill_id_1=skill_note.id,
-#                    skill_proficiency_1=4,
-#                    skill_id_2=1,
-#                    skill_proficiency_2=3,
-#                    skill_id_3=2,
-#                    skill_proficiency_3=2
-#                    )
-# new_project.project_members.append(new_user)
-# new_project.project_members.extend((new_user_2, new_user_3))
-# db.session.commit()
+print("ProjectInterests - for all interests and all projects tagged with interest\n")
+interests_rows = ProjectInterests.query.all()
+for interest in interests_rows:
+    print(f"{interest.__repr__()}")
+    for idx, user in enumerate(interest.users):
+        print(f'\t i={idx}: {user.__repr__()})')
+print("-"*100)
--- webapp.py
+++ webapp.py
@@ -32,8 +32,7 @@ def bootstrap_data():
     print("Initialized and Bootstrapped DB tables with data")
 
 
-# Association table: automatically updates based on Users and Projects tables
-# https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html#many-to-many
+# Association table: automatically updates based on Users and Projects tables (https://docs.sqlalchemy.org/en/14/orm/basic_relationships.html#many-to-many)
 UserProjects = db.Table("userproject",
                         db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
                         db.Column("project_id", db.Integer, db.ForeignKey("project.id"), primary_key=True))
@@ -63,13 +62,21 @@ class Users(db.Model):
     skill_2 = db.relationship("Skills", foreign_keys=skill_id_2)
     skill_3 = db.relationship("Skills", foreign_keys=skill_id_3)
 
+    # CS Career Goal (top choice)
+    careergoal_id = db.Column(db.Integer, db.ForeignKey('csfield.id'), nullable=False, default=1)
+    career = db.relationship("csField", foreign_keys=careergoal_id, backref="users")
+
+    # ProjectInterests (top choice)
+    projectinterest_id = db.Column(db.Integer, db.ForeignKey('projectinterest.id'), nullable=False, default=1)
+    interest = db.relationship("ProjectInterests", foreign_keys=projectinterest_id, backref="users")
+
     def __repr__(self):
         return f"User('{self.first_name}', '{self.last_name}', '{self.email}')"
 
 
 class Projects(db.Model):
     ''' Projects Table: 
-    containg group projects a user can post
+    Group projects a user can post
     '''
     __tablename__ = 'project'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -78,10 +85,25 @@ class Projects(db.Model):
     creation_timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     start_date = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
     target_end_date = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, default=1)
+    owner = db.relationship("Users",
+                            foreign_keys=owner_id,
+                            backref="projects",
+                            uselist=False)
+
     project_members = db.relationship('Users',
                                       secondary=UserProjects,
                                       lazy='subquery',
                                       backref="user_projects")
+
+    # CS Career Goal (top choice)
+    careergoal_id = db.Column(db.Integer, db.ForeignKey('csfield.id'), nullable=False, default=1)
+    career = db.relationship("csField", foreign_keys=careergoal_id, backref="projects")
+
+    # ProjectInterests (top choice)
+    projectinterest_id = db.Column(db.Integer, db.ForeignKey('projectinterest.id'), nullable=False, default=1)
+    interest = db.relationship("ProjectInterests", foreign_keys=projectinterest_id, backref="projects")
 
     def __repr__(self):
         return f"Project[ID:'{self.id}', Name:'{self.name}'] "
@@ -106,10 +128,14 @@ class csField(db.Model):
     Common SWE related roled per StackOverflow 2019 survey
     - examples of csfield_name {backend, frontend, mobile ,devops, etc.}
     '''
+    __tablename__ = 'csfield'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(50), unique=True, nullable=False)
     desc = db.Column(db.String(250), nullable=False)
     image = db.Column(db.String(20), nullable=False, default="csField.jpg")
+
+    def __repr__(self):
+        return f"csField('{self.name}', '{self.desc}')"
 
 
 class ProjectInterests(db.Model):
@@ -118,10 +144,14 @@ class ProjectInterests(db.Model):
     - Industries (ex. Aerospace, Finance, IoT, etc.)
     - General interests in technology (ex. Machine Learning, NLP, Autonomous vehicles)
     '''
+    __tablename__ = 'projectinterest'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(50), unique=True, nullable=False)
     desc = db.Column(db.String(250), nullable=False)
     image = db.Column(db.String(20), nullable=False, default="ProjectInterests.jpg")
+
+    def __repr__(self):
+        return f"ProjectInterests('{self.name}', '{self.desc}')"
 
 
 # ----------------------------------------------------------------------------------------------------------------------------
@@ -162,7 +192,7 @@ def bootstrap_helper():
                    skill_id_2=skill_3.id,
                    skill_proficiency_2=4,
                    skill_id_3=skill_cpp.id,
-                   skill_proficiency_3=2
+                   skill_proficiency_3=2,
                    )
     db.session.add(user_1)
     db.session.add(user_2)
@@ -171,9 +201,11 @@ def bootstrap_helper():
 
     # ---------- Add Projects ----------
     project_1 = Projects(name="Lets make a React App!!!",
-                         desc="Welcome all levels of exp, just looking to get expossure to react")
+                         desc="Welcome all levels of exp, just looking to get expossure to react",
+                         owner=user_2.id)
     project_2 = Projects(name="Anyone looking to get started with mobile development?",
-                         desc="Currently interested in Koitlin dev, but open to other stacks as well!")
+                         desc="Currently interested in Koitlin dev, but open to other stacks as well!",
+                         owner=user_2.id)
     db.session.add(project_1)
     db.session.add(project_2)
 
@@ -195,6 +227,46 @@ def bootstrap_helper():
     interest_2 = ProjectInterests(name="Space", desc="Simulations, Robotics, Computer vision")
     db.session.add(interest_1)
     db.session.add(interest_2)
+    db.session.commit()
+
+    # ---------- Projects - Adding members to projects and skills (Additional exxamples) ----------
+    new_project = Projects(name="Machine learning noobs",
+                           desc="looking for other noobs to develop machine learning skills with")
+    db.session.add(new_project)
+    db.session.commit()
+
+    skill_vim = Skills(name="Vim", desc="A text editor")
+    skill_vs = Skills(name="vscode", desc="A text editor")
+    skill_note = Skills(name="notepad", desc="A text editor")
+    db.session.add_all([skill_vim, skill_vs, skill_note])
+    db.session.commit()
+
+    new_user = Users(first_name='vim', last_name='god', email="vimgod@vim.com",
+                     skill_id_1=skill_vim.id,
+                     skill_proficiency_1=4,
+                     skill_id_2=1,
+                     skill_proficiency_2=3,
+                     skill_id_3=2,
+                     skill_proficiency_3=2
+                     )
+    new_user_2 = Users(first_name='vscode', last_name='champ', email="not_vimgod@vs.com",
+                       skill_id_1=skill_vs.id,
+                       skill_proficiency_1=4,
+                       skill_id_2=1,
+                       skill_proficiency_2=3,
+                       skill_id_3=2,
+                       skill_proficiency_3=2
+                       )
+    new_user_3 = Users(first_name='notepad', last_name='OG', email="whatsvim@og.com",
+                       skill_id_1=skill_note.id,
+                       skill_proficiency_1=4,
+                       skill_id_2=1,
+                       skill_proficiency_2=3,
+                       skill_id_3=2,
+                       skill_proficiency_3=2
+                       )
+    new_project.project_members.append(new_user)
+    new_project.project_members.extend((new_user_2, new_user_3))
     db.session.commit()
 
 
