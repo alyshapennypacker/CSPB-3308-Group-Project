05d77ef99cba78b7e6e441204a7df52ed731d290
Daniel Bae <danieljbaee@hotmail.com>
2021-05-02 02:02:42 -0700

    fixed cli commands, by using context manager (with statement). And pushing current context (app.app_context().push() after app creation and before db initializtion

--- run.py
+++ run.py
@@ -1,34 +1,36 @@
 from flaskapp import create_app
 
-# from flaskapp import app, db
-# from flaskapp.tests.db_tables import bootstrap_helper, query_helper
-
-# @app.cli.command("db-clean")
-# def reset_db():
-#     ''' Drops and Creates fresh database '''
-#     db.drop_all()
-#     db.create_all()
-#     print("Initialized clean DB tables")
-
-
-# @app.cli.command("db-load")
-# def bootstrap_data():
-#     ''' Populates database with some sample data '''
-#     db.drop_all()
-#     db.create_all()
-#     bootstrap_helper(db)
-#     print("Loading data...\n")
-#     print("Re-Initialized and Bootstrapped with sample data")
-
-
-# @app.cli.command("db-query")
-# def query_sampleData():
-#     ''' Queries sample data from `boot` command '''
-#     query_helper(db)
-#     print("Querying Sample Data...")
+from flaskapp import db
+from flaskapp.tests.db_tables import bootstrap_helper, query_helper
 
 app = create_app()
 app.app_context().push()
+
+@app.cli.command("db-clean")
+def reset_db():
+    ''' Drops and Creates fresh database '''
+    db.drop_all()
+    db.create_all()
+    print("Initialized clean DB tables")
+
+
+@app.cli.command("db-load")
+def bootstrap_data():
+    ''' Populates database with some sample data '''
+    db.drop_all()
+    db.create_all()
+    bootstrap_helper(db,app)
+    print("Loading data...\n")
+    print("Re-Initialized and Bootstrapped with sample data")
+
+
+@app.cli.command("db-query")
+def query_sampleData():
+    ''' Queries sample data from `boot` command '''
+    with app.app_context():
+        query_helper(db,app)
+        print("Querying Sample Data...")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
